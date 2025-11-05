b08030fb232a04caab4402377f73e9fd917c3b1c
Zablonm <zablon.mulugeta@gmail.com>
2025-11-04 21:12:27 -0500

    Create student class with constructor
    Define a student class with an initializer.

---
+++ student.py
@@ -0,0 +1,8 @@
+class student:
+
+    #__init__ is a special type of method that works to initlize varriables
+    #instance variables in an object
+    def __init__(self, fn = "None", ln = "None", gr = -1):
+        self.first_name = fn
+        self.last_name = last_name
+        self.grade = gr
